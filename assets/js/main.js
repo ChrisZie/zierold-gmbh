451ae665e46154a143bd102b0d3a0010bd9698bf
ChrisZie <chrissie2788@gmail.com>
2019-08-19 01:30:39 +0700

    add carousel to product overview and add products for that

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,3 +17,24 @@ $(".navbar-toggler").click(function() {
     $(".navbar-collapse").toggleClass("show");
   }, 200);
 });
+
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  responsiveClass:true,
+  responsive:{
+      0:{
+          items:1,
+          nav:true
+      },
+      600:{
+          items:3,
+          nav:false
+      },
+      1000:{
+          items:3,
+          nav:true,
+          loop:false
+      }
+  }
+})
